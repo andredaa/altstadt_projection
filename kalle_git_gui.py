27af0f60a064f19fa3cc1edfccbfcdaa6461ec8c
Andre <al@daa.net>
2019-04-28 21:59:06 +0200

    Add handling in case the git pull was not sucessfull

--- kalle_git_gui.py
+++ kalle_git_gui.py
@@ -1,5 +1,6 @@
 # assuming Python3
 import tkinter as tk
+from tkinter import *
 import subprocess as sub
 
 # TODO set permissions for reboot
@@ -12,32 +13,47 @@ def reboot():
     print(output)
 
 
-def write_slogan():
-    # Git pull im ordner des git repos ausführen
+def update_git():
     with sub.Popen(["git", "pull"], stdout=sub.PIPE) as proc:
         #  output = process.communicate()[0]   #  sub.check_call("touch test")
         output =  proc.stdout.read().decode("utf-8")
         print(output)
 
     # Give the option to either reboot or quit the window
+    # Keine Änderungen
     if "Already up-to-date." in output:
         button = tk.Button(frame,
-                           text="Keine Updates verfügbar, Fenster schließen",
+                           text="Keine Updates verfuegbar, Fenster schliessen",
                            fg="black",
                            command=quit)
-    else:
+    # erfolgreich aktualisiert
+    elif "Updating" in output:
         button = tk.Button(frame,
                            text="Updates empfangen! Jetzt neu starten",
                            fg="green",
                            command=reboot)
+    # es gab warhscheinlich eine Errormeldung
+    else:
+        with sub.Popen(["git", "status"], stdout=sub.PIPE) as proc:
+            #  output = process.communicate()[0]   #  sub.check_call("touch test")
+            output = proc.stdout.read().decode("utf-8")
+            print(output)
+
+        T = Text(root, height=40, width=100)
+        T.pack()
+        T.insert(END, "Da ist eventuell was schief gegangen.\n\n\n", "Foto von Text machen, Andre schicken.\n", output)
+        button = tk.Button(frame,
+                           text="Man kann nix kaputt machen. Wir gucken morgen mal",
+                           fg="black",
+                           command=quit)
+
 
     # display new button
-
     button.pack(side=tk.LEFT)
     root.mainloop()
 
 
-WINDOW_SIZE = "600x400"
+WINDOW_SIZE = "800x600"
 
 root = tk.Tk()
 root.geometry(WINDOW_SIZE)
@@ -46,9 +62,9 @@ frame = tk.Frame(root)
 frame.pack()
 
 refresh_button = tk.Button(frame,
-                   text="Aktualisieren",
-                   fg="black",
-                   command=write_slogan)
+                           text="Aktualisieren",
+                           fg="black",
+                           command=update_git)
 
 refresh_button.pack(side=tk.LEFT)
 
